f312663d7046c1e0a638746f874b29e8086330cc
fash25 <fash25rus@gmail.com>
2022-08-30 16:57:46 +1000

    first example of type casting

--- src/ru/synergy/Main.java
+++ src/ru/synergy/Main.java
@@ -1,9 +1,25 @@
 package ru.synergy;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+
+        Object o = new Scanner(System.in);
+
+        Scanner scanner = null;
+
+        if ( o instanceof Scanner ) scanner = (Scanner) o;
+        if ( scanner != null ){
+            scanner.nextInt();
+        }
+        ((Scanner) o ).nextInt();
+
+        Object o1 = new String();
+
 
     }
 }
