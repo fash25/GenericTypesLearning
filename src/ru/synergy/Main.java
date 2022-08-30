c55bb0871c02dcf8a47cf37882603ac7369cfe89
fash25 <fash25rus@gmail.com>
2022-08-30 17:25:29 +1000

    Finished with Instanseof

--- src/ru/synergy/Main.java
+++ src/ru/synergy/Main.java
@@ -7,18 +7,27 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
+//        Object o = new Scanner(System.in);
+//
+//        Scanner scanner = null;
+//
+//        if ( o instanceof Scanner ) scanner = (Scanner) o;
+//        if ( scanner != null ){
+//            scanner.nextInt();
+//        }
+//        ((Scanner) o ).nextInt();
+//
+//        Object o1 = new String();
 
-        Object o = new Scanner(System.in);
 
-        Scanner scanner = null;
+        Object[] objects = {10,"Hai",3,14};  // автоупаковка
 
-        if ( o instanceof Scanner ) scanner = (Scanner) o;
-        if ( scanner != null ){
-            scanner.nextInt();
+        for (Object o : objects){
+            if ( o  instanceof String ){
+                String s = (String) o;
+                System.out.println(s);
+            }
         }
-        ((Scanner) o ).nextInt();
-
-        Object o1 = new String();
 
 
     }
