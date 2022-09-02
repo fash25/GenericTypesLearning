8781386f51e79a0e474d237164a1bc760bc14a91
fash25 <fash25rus@gmail.com>
2022-09-02 12:59:50 +1000

    Added Account class

---
+++ src/ru/synergy/CustomGenericTypes/Account.java
@@ -0,0 +1,27 @@
+package ru.synergy.CustomGenericTypes;
+
+public class Account {
+    private int id;
+    private int sum;
+
+    public Account(int id, int sum) {
+        this.id = id;
+        this.sum = sum;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getSum() {
+        return sum;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setSum(int sum) {
+        this.sum = sum;
+    }
+}
