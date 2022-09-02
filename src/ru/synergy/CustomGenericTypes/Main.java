69ded48cc7ba4f324aa4a68d75b457cba922afb2
fash25 <fash25rus@gmail.com>
2022-09-02 14:01:13 +1000

    All Full Ass

--- src/ru/synergy/CustomGenericTypes/Main.java
+++ src/ru/synergy/CustomGenericTypes/Main.java
@@ -1,7 +1,21 @@
 package ru.synergy.CustomGenericTypes;
 
 public class Main {
+
     public static void main(String[] args){
+
+
+        Account acc1 = new Account( id 12345, sum 5000);
+
+        System.out.println(acc1.getId());
+
+        Account acc2 = new Account( id "sid12345", sum 5000);
+
+        System.out.println(acc2.getId());
+
+        int a = ((Integer) acc1.getId()) + 10;
+
+
 
     }
 }
